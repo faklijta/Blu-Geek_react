4b582a1541e0478689cc6ccdc1cb79d18fe3a950
faklijta <faklijta@gmail.com>
2018-02-08 11:53:43 +0100

    fix(header) const

--- blugeek-blog/src/layouts/index.js
+++ blugeek-blog/src/layouts/index.js
@@ -7,35 +7,6 @@ import Media from 'react-media'
 import './index.css'
 import "../styles/layout-overide.css"
 
-// const Header = () => (
-//   <div
-//     style={{
-//       background: '#f5f5f5',
-//       marginBottom: '3rem',
-//       borderBottom: '2px solid #e6e6e6',
-//     }}
-//   >
-//     <div
-//       style={{
-//         margin: '0 auto',
-//         maxWidth: 980,
-//         padding: '1.45rem 1.0875rem',
-//       }}
-//    >
-//      <h1 style={{margin: 0, textAlign: 'center',fontSize: '18px'}}>
-//         <Link to="/"
-//           style={{
-//             color: 'black',
-//             textDecoration: 'none',
-//           }}
-//         >
-//           CodeStack
-//         </Link>
-//       </h1>
-//     </div>
-//   </div>
-// );
-
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
@@ -104,6 +75,35 @@ const TemplateWrapper = ({ children }) => (
     </div>
   </div>
 );
+
+// const Header = () => (
+//   <div
+//     style={{
+//       background: '#f5f5f5',
+//       marginBottom: '3rem',
+//       borderBottom: '2px solid #e6e6e6',
+//     }}
+//   >
+//     <div
+//       style={{
+//         margin: '0 auto',
+//         maxWidth: 980,
+//         padding: '1.45rem 1.0875rem',
+//       }}
+//    >
+//      <h1 style={{margin: 0, textAlign: 'center',fontSize: '18px'}}>
+//         <Link to="/"
+//           style={{
+//             color: 'black',
+//             textDecoration: 'none',
+//           }}
+//         >
+//           CodeStack
+//         </Link>
+//       </h1>
+//     </div>
+//   </div>
+// );
 
 const Sidebar = (props) => (
   <div
